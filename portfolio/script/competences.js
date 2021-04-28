eed7d1e612566c158138148b3cc4c8c2918d3968
Bastien Pateyron <bastien.pateyron@gmail.com>
2021-04-28 16:32:46 +0200

    Flip cards acceptables

---
+++ portfolio/script/competences.js
@@ -0,0 +1,20 @@
+/*
+* Hide all the cards
+*/
+function hideAllCards() {
+    var cardTextList = document.getElementsByClassName('card-text')
+    for(const card of cardTextList) card.style.display = 'none'
+}
+
+/*
+* @param id: The id of the selected card
+* Hide all flipped card first
+*/
+function displayCard(id) {
+    console.log(id)
+    hideAllCards()
+    // querySelector sélectionne le premier match de la classe 'card-text' parmi l'élement de notre id
+    var text = document.getElementById(id).querySelector('.card-text')
+    if (["''", 'none'].includes(text.style.display) ) text.style.display = 'block'
+    else text.style.display = 'none'
+}
